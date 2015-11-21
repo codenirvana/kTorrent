279ae0e7dbf8f70310c7f813104481ec26f864c4
Udit <codenirvana28@gmail.com>
2015-11-21 13:14:29 +0530

    improved test

--- ktorrent/test.py
+++ ktorrent/test.py
@@ -1,12 +1,16 @@
 from unittest import TestCase
 
-import ktorrent
+import ktorrent, json
 
-class FailureMessageTest(TestCase):
+class kTorrentTest(TestCase):
 
-    def testFail(self):
+    def testConnection(self):
         output = ktorrent.search(search='linux')
         self.failIfEqual(output,"Couldn't retrieve data.")
+
+    def testOutput(self):
+        output = ktorrent.search(search='linux')
+        self.assertEqual( type( json.loads(output) ) is dict , True)
 
 if __name__ == '__main__':
     unittest.main()
