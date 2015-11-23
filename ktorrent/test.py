12a78cc254134a96c61e169e6487f9cabb21c9aa
Udit Vasu <codenirvana28@gmail.com>
2015-11-23 12:51:28 +0530

    doced test

--- ktorrent/test.py
+++ ktorrent/test.py
@@ -5,10 +5,12 @@ import ktorrent, json
 class kTorrentTest(TestCase):
 
     def testConnection(self):
+        # Check connection
         output = ktorrent.search(search='linux')
         self.failIfEqual(output,"Couldn't retrieve data.")
 
     def testOutput(self):
+        # Validate JSON
         output = ktorrent.search(search='linux')
         self.assertEqual( type( json.loads(output) ) is dict , True)
 
