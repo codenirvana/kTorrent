f15390d5f69e3cff275d9769253704072df3769b
Udit <codenirvana28@gmail.com>
2015-11-27 12:08:15 +0530

    release 0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ktorrent',
-    version='0.3.0',
+    version='0.3.1',
     description='Fetches and parses data from Kickass Torrents.',
     license='MIT',
     author='Udit Vasu',
