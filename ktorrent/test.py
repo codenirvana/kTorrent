95643619720fe433a87e73b27fd0c93bdfe5134d
Udit <codenirvana28@gmail.com>
2016-01-12 22:30:30 +0530

    changed tests function name

--- ktorrent/test.py
+++ ktorrent/test.py
@@ -9,12 +9,12 @@ class kTorrentTest(TestCase):
         output = ktorrent.search(search='linux')
         self.failIfEqual(output,"Couldn't retrieve data")
 
-    def testOutput1(self):
+    def testSearch(self):
         # search: Validate JSON
         output = ktorrent.search(search='linux')
         self.assertEqual( type( json.loads(output) ) is dict , True)
 
-    def testOutput2(self):
+    def testTop(self):
         # top: Validate JSON
         output = ktorrent.top(category='movies', page='2')
         self.assertEqual( type( json.loads(output) ) is dict , True)
