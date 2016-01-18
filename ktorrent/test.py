0bf2dc02eb623946726d7660337d89c5c1c93ca3
Udit <admin@codenirvana.in>
2016-01-18 22:20:11 +0530

    test for invalid args

--- ktorrent/test.py
+++ ktorrent/test.py
@@ -19,5 +19,14 @@ class kTorrentTest(TestCase):
         output = ktorrent.top(category='movies', page=2)
         self.assertEqual( type( json.loads(output) ) is dict , True)
 
+    def testInvalidArgs(self):
+        # Check invalid args
+        output1 = ktorrent.search(search='Linux', strict=404, category='book')
+        output2 = ktorrent.search(category='books')
+        output3 = ktorrent.top(category='movies', page='2')
+        self.assertEqual(output1, "Invalid parameters passed")
+        self.assertEqual(output2, "Invalid parameters passed")
+        self.assertEqual(output3, "Invalid parameters passed")
+
 if __name__ == '__main__':
     unittest.main()
